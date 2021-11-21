c765c5a4cd6c35d983c5acbfd7fa2f1b591d96e0
rjc123451 <1533926589@qq.com>
2021-11-21 17:06:43 +0800

    新一次

---
+++ src/main/java/Add.java
@@ -0,0 +1,35 @@
+public class Add
+{
+    int a;
+    int b;
+    int c;
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    public int getB() {
+        return b;
+    }
+
+    public void setB(int b) {
+        this.b = b;
+    }
+
+    public int getC() {
+        return c;
+    }
+
+    public void setC(int c) {
+        this.c = c;
+    }
+
+    int add(int a, int b){
+       c=a+b;
+       return c;
+   }
+}
--- src/main/java/test.java
+++ src/main/java/test.java
@@ -1,8 +1,10 @@
 public class test {
     public static void main(String[] args) {
-        int a,b;
-        a=5;
-        b=6;
-        System.out.println(a+b);
+        Add s=new Add();/*实现两个数相加*/
+        int sum;
+        s.setA(5);
+        s.setB(6);
+        sum=s.add(s.getA(), s.getB());
+        System.out.println(sum);
     }
 }
